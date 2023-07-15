6bf21186ee5f9861f4ffd9a094a4bd6ea90ba9eb
Nikhil-Panda <pandanikhil0408@gmail.com>
2023-07-15 14:59:58 +0530

    Weather App

---
+++ Weather.py
@@ -0,0 +1,24 @@
+import requests
+
+API_KEY = "cae13eb3685cc9a3a032eb2c2d86c80f"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a City Name:")
+request_url = f"{BASE_URL}?q={city}&appid={API_KEY}"
+#forming the api link to fetch data about the city's weather
+
+response = requests.get(request_url)
+#sending a get request to the url formed above
+
+if response.status_code == 200:
+    data = response.json()
+    weather=data['weather'][0]['description']
+    print("Weather:", weather)
+    temparature = round(data['main']['temp'] - 273.15, 2)
+    print("Temparature:",temparature,"Celsius")
+else:
+    print("Error Occured")
+    print("status code:", response.status_code)
+    print("response content", response.content)
+    
+
